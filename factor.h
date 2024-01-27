5b875970733e11142b2d94e10635f02e1cc24c8a
Mackay89 <manotomackay@gmail.com>
2024-01-26 23:30:04 -0800

    factors

---
+++ factor.h
@@ -0,0 +1,13 @@
+#ifndef _FACTOR_H
+#define _FACTOR_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+/* size_t getline(char == lineptr, size_t *n FILE *stream); */
+int factorize(char *buffer);
+
+#endif /* _FACTOR_H */
+	
+
